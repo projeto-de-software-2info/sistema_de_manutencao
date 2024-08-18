c552cf9c8ccdce006ff5580aef804f64c7137448
fujulia <juliafu713@gmail.com>
2024-08-17 22:06:25 -0300

    FEAT: Pagina de login criada #1

---
+++ src/components/Login/form/button/EnviarButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(['titulo'])
+</script>
+<template>
+  <button type="submit">{{ titulo }}</button>
+</template>
+<style scoped>
+button {
+  width: 100%;
+  background-color: #012340;
+  border-radius: 8px;
+  height: 35px;
+  color: white;
+  box-shadow: none;
+  outline: none;
+  border: 1px solid #012340;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+button:hover {
+  background-color: #002c52;
+}
+</style>
