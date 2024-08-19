6ed5f26cbbe191c2dfb165e35edbeb2fdc57f1af
isabellirosa <isabelli.ifc@gmail.com>
2024-08-18 22:35:58 -0300

    FEAT: Criando o navbar, rotas e views #2

---
+++ src/components/Navbar/DefaultNavbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import DefaultLinks from './links/DefaultLinks.vue'
+import NavbarLogo from './logo/NavbarLogo.vue'
+import NavbarFechar from './fechar/NavbarFechar.vue'
+</script>
+<template>
+  <nav>
+    <NavbarLogo />
+    <DefaultLinks />
+    <NavbarFechar />
+  </nav>
+</template>
+<style scoped>
+nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  background-color: white;
+  width: 20vw;
+  height: 100vh;
+  position: fixed;
+  padding: 50px;
+}
+</style>
