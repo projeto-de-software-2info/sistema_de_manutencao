cd5e9d4975141d63da3e0a7c6cffc48f25bb94e5
analauradias <analauramanfron@gmail.com>
2024-08-25 16:08:07 -0300

    FEAT: Página de orçamento criada #8

---
+++ src/components/PagOrcamento/DefaultPagOrcamento.vue
@@ -0,0 +1,25 @@
+<script setup>
+        import HeaderOrcamento from './titulo/HeaderOrcamento.vue';
+        import FiltroOrcamento from './filtro/FiltroOrcamento.vue';
+        import InfoClienteOrcamento from './cliente/InfoClienteOrcamento.vue';
+</script>
+
+<template>
+    <main>
+        <HeaderOrcamento />
+        <FiltroOrcamento /> 
+        <InfoClienteOrcamento />
+    </main>
+</template>
+
+<style scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(243, 243, 243, 1);
+    margin-left: 20vw;
+    height: 100vh;
+    }
+</style>
